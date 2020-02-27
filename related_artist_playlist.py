80efe71381f3c89bf39e0f6abd63a5aa58379bcd
boris <boris@appsflyer.com>
2020-02-27 23:29:46 +0200

    Add some config params, fix playlist 100 tracks limit per request

--- related_artist_playlist.py
+++ related_artist_playlist.py
@@ -1,34 +1,41 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 import sys
+import argparse
 import pprint
+from spotipy.oauth2 import SpotifyOAuth
 
-client = None
+spotify_username = "SPOTIFY_USERNAME"
+TRACKS_PER_RELATED_ARTIST = 2
 
+spotify_client = None
 track_list = []
 track_name_list = []
-username = SPOTIFY_USER_NAME
 
 def authenticate():
 	scope = "playlist-modify-public"
-	sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope,username=username))
+	sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope,username=spotify_username))
 	return sp
 
+def chunks(list, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(list), n):
+        yield list[i:i + n]
+
 def get_artist_uri(artist_name):
-	result = client.search(q='artist:' + artist_name, type='artist')
+	result = spotify_client.search(q='artist:' + artist_name, type='artist')
 	artist = result['artists']['items'][0]
 	name = artist['name']
 	uri = artist['uri']
 	return uri
 
 def add_artist_top_track_to_tracklist(artist_uri):
-	response = client.artist_top_tracks(artist_uri)
-	for track in response['tracks'][0:2]:
+	response = spotify_client.artist_top_tracks(artist_uri)
+	for track in response['tracks'][0:TRACKS_PER_RELATED_ARTIST]:
 		track_list.append(track['uri'])
 		track_name_list.append(track['name'])
 
 def get_related_artists(artist_uri):
-	related = client.artist_related_artists(artist_uri)
+	related = spotify_client.artist_related_artists(artist_uri)
 
 	for related_artist in related['artists']:
 		print('  ', related_artist['name'])
@@ -39,31 +46,48 @@ def get_related_artists(artist_uri):
 	#print(track_name_list)
 
 def create_playlist(artist_name):
-	playlist_name = "Friends of " + artist_name
+	playlist_name = artist_name + " - Related Artsits Playlist"
 	print("Creating playlist: " + playlist_name)
 
-	new_playlist = client.user_playlist_create(username, playlist_name)
+	new_playlist = spotify_client.user_playlist_create(spotify_username, playlist_name)
 	playlist_id = new_playlist['id']
-	results = client.user_playlist_add_tracks(username, playlist_id, track_list)
-	print(results)
+	split_track_list = chunks(track_list, 100)
+	for track in split_track_list:
+		results = spotify_client.user_playlist_add_tracks(spotify_username, playlist_id, track)
 		
-def main():
-	global client
+def main(args):
+	global spotify_client
 	
-	if len(sys.argv) > 1:
-		artist_name = sys.argv[1]
-	else:
-		artist_name = 'metallica'
+	print("Tracks per artist: " + str(TRACKS_PER_RELATED_ARTIST))
+	print("Spotify username: " + spotify_username)
 
-	print("Getting track list for: {}".format(artist_name))
-
-	client = authenticate()
-
+	artist_name = args.artist
+	spotify_client = authenticate()
 	artist_uri = get_artist_uri(artist_name)
+
+	print("Getting related artists")
 
 	get_related_artists(artist_uri)
 
+	print("Getting track list for: {}".format(artist_name))
+
 	create_playlist(artist_name)
 
-main()
+def setArgs(args):
+	global spotify_username
+	global TRACKS_PER_RELATED_ARTIST
+
+	if args.username:
+		spotify_username = args.username
+	if (args.tracks_per_artist):
+		TRACKS_PER_RELATED_ARTIST = args.tracks_per_artist
+
+parser = argparse.ArgumentParser(description="Create a plylist for top tracks of related artists")
+parser.add_argument("artist", type=str, help='Artist name')
+parser.add_argument("-u", "--username", type=str, help='Spotify username')
+parser.add_argument("-t", "--tracks_per_artist", type=int, help='number of tracks to take from artist. Default is 2')
+args = parser.parse_args()
+
+setArgs(args)
+main(args)
 
